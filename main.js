9babb3ed128a77cdb3bd4133beeedabefa056b0e
GayathriBandi <153480381+GayathriBandi@users.noreply.github.com>
2024-06-10 19:44:03 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timerRef = document.querySelector(".timer-display");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(displayTimer, 10);
+});
+
+document.getElementById("pause-timer").addEventListener("click", () => {
+  clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+  clearInterval(int);
+  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+  timerRef.innerHTML = "00 : 00 : 00 : 000";
+  document.getElementById("laps").innerHTML = "";
+});
+
+document.getElementById("lap-timer").addEventListener("click", () => {
+  let lapTime = `${String(hours).padStart(2, "0")} : ${String(minutes).padStart(2, "0")} : ${String(seconds).padStart(2, "0")} : ${String(milliseconds).padStart(3, "0")}`;
+  let lapList = document.getElementById('laps');
+  let listItem = document.createElement('li');
+  listItem.innerText = `Lap ${lapList.childElementCount + 1}: ${lapTime}`;
+  lapList.appendChild(listItem);
+});
+
+function displayTimer() {
+  milliseconds += 10;
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hours++;
+      }
+    }
+  }
+
+  let h = String(hours).padStart(2, "0");
+  let m = String(minutes).padStart(2, "0");
+  let s = String(seconds).padStart(2, "0");
+  let ms = String(milliseconds).padStart(3, "0");
+
+  timerRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
